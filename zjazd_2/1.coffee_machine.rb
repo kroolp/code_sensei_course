ac727998968e0924ffdae18ebc3a738ee957a86e
Piotr Król <piotr.krol@codesthq.com>
2018-07-07 13:48:29 +0200

    move methods to private section

--- zjazd_2/1.coffee_machine.rb
+++ zjazd_2/1.coffee_machine.rb
@@ -1,16 +1,18 @@
 class CoffeeMachine
+  def make_coffee
+    grind_grains
+    puts "Making coffee"
+    add_milk
+  end
+
+  private
+
   def grind_grains
     puts "Grinding grains"
   end
 
   def add_milk
     puts "Adding milk"
-  end
-
-  def make_coffee
-    grind_grains
-    puts "Making coffee"
-    add_milk
   end
 end
 
